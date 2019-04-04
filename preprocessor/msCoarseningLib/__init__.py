235bb802eb6928c09e181ae2a0a8da1411e94853
Renata Tavares <renatattavares@hotmail.com>
2019-04-04 10:58:16 -0300

    Cleaning master

--- preprocessor/msCoarseningLib/__init__.py
+++
@@ -1,7 +0,0 @@
-#Multiscale preprocessors for reservoir simulation using PRESTO.
-
-print('Partitioning module initialized')
-
-#__all__ = ["readConfig","algoritmo"]
-#import meshManager
-#print(__name__)
