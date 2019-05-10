9a1588a1f6f7236adc85af95ec8ae211fe7e9219
Artur Castiel <arturcastiel@gmail.com>
2019-05-10 14:23:01 -0300

    created GraphMesh class, with predecessor attribute, and path method - need to be throughly tested

--- preprocessor/meshHandle/dualCoarseMesh.py
+++ preprocessor/meshHandle/dualCoarseMesh.py
@@ -8,7 +8,10 @@ from . corePymoab import CoreMoab
 from . meshComponents import MeshEntities
 import hdmedians as hd
 import numpy as np
+import scipy as sp
 import scipy.spatial
+import itertools
+
 print('Dual Coarse Mesh Module initialized')
 
 
@@ -17,6 +20,7 @@ class DualCoarseMesh:
         self.M = M
         self.find_primal_coarse_centers()
         self.find_interface_centers()
+        self.find_vol_neighbors_to_interface_center()
         self.find_coarse_edges()
         self.find_coarse_faces()
         self.find_coarse_volumes()
@@ -36,27 +40,66 @@ class DualCoarseMesh:
             index += 1
 
     def find_interface_centers(self):
-        self.center_face = []
+        #self.center_face = []
         coords = self.M.faces.center[:]
+        self.center_face = np.zeros((len(self.M.coarse.interfaces_faces), 1)).astype("uint64")
+        index = 0
         for cface in self.M.coarse.interfaces_faces:
             tcoords = coords[cface].reshape(len(cface),3)
-            u = int(cface[hd.medoid(tcoords,axis=0,indexonly=True)].ravel())
+            #u = int(cface[hd.medoid(tcoords,axis=0,indexonly=True)].ravel())
             #import pdb; pdb.set_trace()
-            self.center_face.append(u)
-            #self.center_face.append(cface[hd.medoid(tcoords,axis=0,indexonly=True) ] )
+            #self.center_face.append(u)
+            center_coord = np.array(hd.geomedian(tcoords.T)).reshape((1,3))
+            distance = scipy.spatial.distance.cdist(tcoords, center_coord)
+            tag = np.nonzero(distance == distance.min())[0]
+            if len(tag) != 1:
+                tag = tag[0]
+            # u =
+            # print(u)
+            self.center_face[index] = int(cface[tag].ravel())
+            #self.center_face.append(u)
+            index += 1
 
-            #np.array(hd.geomedian(coarse_volume.faces.center[coarse_volume.faces.boundary].T))
+    def find_vol_neighbors_to_interface_center(self):
+        # internal faces
+        internal_volumes = self.M.faces.bridge_adjacencies(self.center_face[0:self.M.coarse.num_internal_faces], interface ="faces", target = "volumes").ravel()
 
+        external_volumes = self.M.faces.bridge_adjacencies(self.center_face[self.M.coarse.num_internal_faces:], interface ="faces", target = "volumes").ravel()
+        #import pdb; pdb.set_trace()
+        self.interface_vol = np.unique(np.hstack((internal_volumes, external_volumes)).astype("uint64"))
 
-        pass
     def find_coarse_edges(self):
-        pass
+        coarse_reference = self.M.coarse.partition[self.interface_vol]
+        edges = []
+        for x in range(len(self.M.coarse.elements)):
+            element_target= self.M.coarse.father_to_local_id(self.interface_vol[np.where(coarse_reference == x)[0]], "volumes", x)
+            element_center = self.M.coarse.father_to_local_id(self.coarse_center[x], "volumes", x)
+            shortest = GraphMesh(self.M.coarse.elements[x])
+            print(x)
+            print(edges)
+            import pdb; pdb.set_trace()
+            [edges.append(shortest.path(element_center, el)) for el in element_target]
+
+        self.coarse_edges = np.array(edges)
+
     def find_coarse_faces(self):
         pass
     def find_coarse_volumes(self):
         pass
 
 class GraphMesh:
-    def __init__(self, M):
-        print("Teste")
-        print(M)
+    def __init__(self, N):
+        graph = self.create_sparse_matrix(N.faces.bridge_adjacencies(N.faces.internal, interface="faces", target="volumes"), len(N.volumes))
+        self.dist_matrix, self.predecessors = sp.sparse.csgraph.shortest_path(graph, directed=False, return_predecessors = True)
+
+    def create_sparse_matrix(self, graph_edges, size_vol):
+        sparse_matrix = sp.sparse.coo_matrix((np.ones((len(graph_edges),),dtype=bool).T, (graph_edges[:, 0], graph_edges[:, 1])), shape=(size_vol, size_vol))
+        return sparse_matrix
+
+    def path(self, source, target):
+        index = target
+        path = []
+        while index != source:
+            path.append(index)
+            index = self.predecessors[0, index]
+        return np.array(path)
--- tests.py
+++ tests.py
@@ -1,11 +1,19 @@
 M.happiness[:] = -1
+for x in range(len(M.coarse.interfaces_nodes)):
+    M.happiness[M.coarse.interfaces_nodes[x]] = x
 
 
 
 M.joy[:] = -1
-
+for x in range(len(M.coarse.interfaces_edges)):
+    M.joy[M.coarse.interfaces_edges[x]] = x
 
 
 M.pride[:] = -1
+for x in range(len(M.coarse.interfaces_faces)):
+    M.pride[M.coarse.interfaces_faces[x]] = x
+
 
 M.dreams[:] = -1
+for x in range(len(M.coarse.interfaces_faces)):
+    M.pride[M.coarse.interfaces_faces[x]] = x
