365c830a1afa7b68d23356f99da920c9ed3921ea
gabrielmmats <gmm4@cin.ufpe.br>
2019-08-07 16:08:51 -0300

    implements 2d skinner

--- preprocessor/meshHandle/corePymoab.py
+++ preprocessor/meshHandle/corePymoab.py
@@ -69,18 +69,15 @@ class CoreMoab:
         print("Entering skinner test")
 
         if self.dimension == 3:
-            faces_on_skin_handles = self.skin.find_skin(self.root_set, self.all_volumes[:])
+            faces_on_skin_handles = self.skin.find_skin(self.root_set, self.all_volumes)
             edges_on_skin_handles = self.mtu.get_bridge_adjacencies(faces_on_skin_handles, 2, 1)
             nodes_on_skin_handles = self.mtu.get_bridge_adjacencies(faces_on_skin_handles, 2, 0)
             volumes_on_skin_handles = self.mtu.get_bridge_adjacencies(faces_on_skin_handles, 0, 3)
         elif self.dimension == 2:
-            edges_on_skin_handles = skin.find_skin(self.root_set, self.all_faces[:])
-            nodes_on_skin_handles = self.access_handle(edges_on_skin_handles)
-            nodes_in_faces = ([self.mb.get_adjacencies(el_handle,0) for el_handle in self.all_faces])
-            check_faces= np.asarray([rng.intersect(el_handle,nodes_on_skin_handles) for el_handle in nodes_in_faces])
-            external_faces_index = np.array([el_handle.empty() for el_handle in check_faces]).astype(bool)
-            faces_on_skin_handles = self.range_index(np.bitwise_not(external_faces_index),self.all_faces)
-            volumes_on_skin_handles = rng.Range()
+            edges_on_skin_handles = self.skin.find_skin(self.root_set, self.all_faces)
+            nodes_on_skin_handles = self.mtu.get_bridge_adjacencies(edges_on_skin_handles, 1, 0)
+            faces_on_skin_handles = self.mtu.get_bridge_adjacencies(edges_on_skin_handles, 0, 2)
+            volumes_on_skin_handles = self.mtu.get_bridge_adjacencies(edges_on_skin_handles, 0, 3) #empty
 
         print("Skinning Operation Successful")
         return [nodes_on_skin_handles, edges_on_skin_handles, faces_on_skin_handles, volumes_on_skin_handles]
--- preprocessor/meshHandle/mscorePymoab.py
+++ preprocessor/meshHandle/mscorePymoab.py
@@ -82,20 +82,17 @@ class MsCoreMoab(CoreMoab):
             volumes_on_skin_handles = rng.Range()
         return [nodes_on_skin_handles, edges_on_skin_handles, faces_on_skin_handles, volumes_on_skin_handles]
     def skinner_operation(self):
+        self.skin = sk.Skinner(self.mb)
         if self.dimension == 3:
-            self.skin = sk.Skinner(self.mb)
             faces_on_skin_handles  = self.skin.find_skin(0, self.all_volumes)
             edges_on_skin_handles = self.mtu.get_bridge_adjacencies(faces_on_skin_handles, 2, 1)
             nodes_on_skin_handles = self.mtu.get_bridge_adjacencies(faces_on_skin_handles, 2, 0)
             volumes_on_skin_handles = rng.intersect(self.mtu.get_bridge_adjacencies(faces_on_skin_handles, 0, 3), self.all_volumes)
         elif self.dimension == 2:
-            edges_on_skin_handles = self.bridge_adjacencies(self.all_edges,self.dimension)
-            nodes_on_skin_handles = self.access_handle(edges_on_skin_handles)
-            nodes_in_faces = ([self.mb.get_adjacencies(el_handle,0) for el_handle in self.all_faces])
-            check_faces= ([rng.intersect(el_handle,nodes_on_skin_handles) for el_handle in nodes_in_faces])
-            external_faces_index = np.array([el_handle.empty() for el_handle in check_faces]).astype(bool)
-            faces_on_skin_handles = self.range_index(np.bitwise_not(external_faces_index),self.all_faces)
-            volumes_on_skin_handles = rng.Range()
+            edges_on_skin_handles = self.skin.find_skin(0, self.all_faces)
+            nodes_on_skin_handles = self.mtu.get_bridge_adjacencies(edges_on_skin_handles, 1, 0)
+            faces_on_skin_handles = rng.intersect(self.mtu.get_bridge_adjacencies(edges_on_skin_handles, 0, 2), self.all_faces)
+            volumes_on_skin_handles = self.mtu.get_bridge_adjacencies(edges_on_skin_handles, 0, 3) #empty
         return [nodes_on_skin_handles, edges_on_skin_handles, faces_on_skin_handles, volumes_on_skin_handles]
 
     def bridge_adjacencies(self, handle, dim):
