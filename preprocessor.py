50fb3170a5e080c51a7d2695e5bd9b4c28d82996
Filipe Cumaru <facsa@cin.ufpe.br>
2020-12-07 10:31:17 -0300

    refactor: tagging interfaces for visualization.

--- preprocessor.py
+++ preprocessor.py
@@ -11,16 +11,20 @@ from pymoab import core, types, rng, topo_util, skinner, tag
 import numpy as np
 import time
 from preprocessor.meshHandle.multiscaleMesh import FineScaleMeshMS as msh
+from preprocessor.meshHandle.finescaleMesh import FineScaleMesh
 # from preprocessor.meshHandle.dualCoarseMesh import DualCoarseMesh as dual
 import preprocessor.geoUtil.geoTools as gtool
 
 start = time.time()
-M = msh('mesh/fivespot105u.msh', dim=2)
-# M = msh('mesh/icecream.msh', dim=3)
-M.core.print(file='preprocessed_icream', extension='.vtk')
-for n, coarse_elem in zip(np.arange(len(M.coarse.elements)), M.coarse.elements):
-    print("writing element {}".format(n+1))
-    coarse_elem.core.print(file='preprocessed_icecream_{}'.format(n), extension='.vtk')
-# M.coarse.elements[0].core.print(file='teste_coarse_element', extension='.vtk')
+M = msh('mesh/eyem.msh', dim=2)
+
+faces = M.core.all_faces[:]
+ifaces = M.coarse.all_interface_faces[:]
+iface_tag = M.core.mb.tag_get_handle("iface", size=1, tag_type=types.MB_TYPE_INTEGER, 
+                                    storage_type=types.MB_TAG_SPARSE, create_if_missing=True)
+M.core.mb.tag_set_data(iface_tag, faces[ifaces], np.ones(ifaces.size, dtype=int))
+
+M.core.print(file='preprocessed_eyem', extension='.vtk')
+
 end = time.time()
 print("The preprocessing step lasted {0}s".format(end-start))
