f8d2a898cc1c6c8d6df54fa0a850171392fc6765
Filipe Cumaru <facsa@cin.ufpe.br>
2020-12-30 18:37:40 -0300

    chore: add comments

--- preprocessor/element_order/BaseOrdering.py
+++ preprocessor/element_order/BaseOrdering.py
@@ -6,8 +6,13 @@ class BaseOrdering(object):
     """
     
     def __init__(self, mesh_entities, target_dim, interface_dim):
+        # A IMPRESS MeshEntities object.
         self.mesh_entities = mesh_entities
+
+        # The dimension of the elements to be ordered.
         self.target_dim = target_dim
+
+        # The dimension of the interface to check for neighbors.
         self.interface_dim = interface_dim
     
     def sort_elements(self):
