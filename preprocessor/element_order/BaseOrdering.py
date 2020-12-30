633db342d4c37b2b7550efadca8d4ddec35d062a
Filipe Cumaru <facsa@cin.ufpe.br>
2020-12-30 12:43:54 -0300

    refactor: rename attribute and fix identation

--- preprocessor/element_order/BaseOrdering.py
+++ preprocessor/element_order/BaseOrdering.py
@@ -1,14 +1,14 @@
 class BaseOrdering(object):
-  """
-  Base class that defines the basic methods and
-  attributes for element sorting. It should NOT
-  be instantiated.
-  """
-  
-  def __init__(self, mesh, target_dim, interface_dim):
-    self.mesh = mesh
-    self.target_dim = target_dim
-    self.interface_dim = interface_dim
-  
-  def sort_elements(self):
-    raise NotImplementedError
+    """
+    Base class that defines the basic methods and
+    attributes for element sorting. It should NOT
+    be instantiated.
+    """
+    
+    def __init__(self, mesh_entities, target_dim, interface_dim):
+        self.mesh_entities = mesh_entities
+        self.target_dim = target_dim
+        self.interface_dim = interface_dim
+    
+    def sort_elements(self):
+        raise NotImplementedError
